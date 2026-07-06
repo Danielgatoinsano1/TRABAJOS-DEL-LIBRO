f179dc124de720356cd16d56fae69516b927d5f7
Danielgatoinsano1 <danielddd173@gmail.com>
2026-07-06 17:29:21 +0200

    Agregar menu principal

---
+++ misitiof/menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./imgfares/favicon.png" type="image/png" sizes="16x16">
+    <link rel="stylesheet" type="text/css" href="./EstilosCssF/dcoloresf.css">
+    <link rel="stylesheet" type="text/css" href="./EstilosCssF/w3.css">
+    <script src="./JScript/acciones_script.js"></script>
+
+    <title>Inventario Fares</title>
+</head>
+<body>    
+
+<div class="w3/container">
+    <header class="w3-container fcolor-d5">
+    <h1>Ediciones Fares</h1>
+
+    </header>
+</div>
+
+</body>
+</html>
