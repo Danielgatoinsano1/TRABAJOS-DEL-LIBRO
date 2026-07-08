0d117bff21b2c28fc6d51a52fc9453c0dd792f98
Danielgatoinsano1 <danielddd173@gmail.com>
2026-07-08 15:26:01 +0200

    Actualizar menu principal

--- misitiof/menu.php
+++ misitiof/menu.php
@@ -5,46 +5,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="./imgfares/favicon.png" type="image/png" sizes="16x16">
-    <link rel="stylesheet" type="text/css" href="./EstilosCssF/dcoloresf.css">
-    <link rel="stylesheet" type="text/css" href="./EstilosCssF/w3.css">
-    <script src="./JScript/acciones_script.js"></script>
 
-    <title>Inventario Fares</title>
+    <title>Ediciones Fares</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="EstilosCssF/w3.css">
+    <link rel="stylesheet" href="EstilosCssF/dcoloresf.css">
+
+    <!-- JavaScript -->
+    <script src="JScript/acciones_script.js"></script>
 </head>
-<body>    
 
-<div class="w3/container">
+<body>
+
+<div class="w3-container">
+
     <header class="w3-container fcolor-d5">
-    <h1>Ediciones Fares</h1>
-
+        <h1>Ediciones Fares</h1>
     </header>
-    <nav class="w3-bar fcolor-14">
-        <a href="#" class="w3-bar-item w3-button w3-mobile">Principal</a>
-        <div class="w3-container">
-            <button>Libros</button>
-            <div>
-                <a href="#">Educacion Basica</a>
-                <a href="#">I BTP y I  BCH</a>
-                <a href="#">II BTP</a>
-                <a href="#">III BPT</a>
 
+    <nav class="w3-bar fcolor-l4">
+
+        <a href="#" class="w3-bar-item w3-button w3-mobile">
+            Principal
+        </a>
+
+        <div class="w3-dropdown-hover w3-mobile">
+            <button class="w3-button">Libros</button>
+
+            <div class="w3-dropdown-content w3-bar-block ftema">
+                <a href="#" class="w3-bar-item w3-button">Educación Básica</a>
+                <a href="#" class="w3-bar-item w3-button">I BTP y I BCH</a>
+                <a href="#" class="w3-bar-item w3-button">II BTP</a>
+                <a href="#" class="w3-bar-item w3-button">III BTP</a>
             </div>
         </div>
-        <div>
-            <button>Inventario</button>
-            <div>
-                <a href="cproducots.php">Crear produco nuevo</a>
-                <a href="#">Consultar y modificar producto</a>
-                <a href="#">Clientes</a>
-                <a href="#">Agregar Inventario</a>
-                <a href="#">Facturar</a>
-                <a href="#">Informacion Libros</a>
+
+        <div class="w3-dropdown-hover w3-mobile">
+            <button class="w3-button">Inventario</button>
+
+            <div class="w3-dropdown-content w3-bar-block ftema">
+                <a href="cproductos.php" class="w3-bar-item w3-button">Crear producto</a>
+                <a href="#" class="w3-bar-item w3-button">Consultar y modificar producto</a>
+                <a href="#" class="w3-bar-item w3-button">Clientes</a>
+                <a href="#" class="w3-bar-item w3-button">Agregar inventario</a>
+                <a href="#" class="w3-bar-item w3-button">Facturar</a>
+                <a href="#" class="w3-bar-item w3-button">Información libros</a>
             </div>
         </div>
-        <a href="#">Contacto</a>
+
+        <a href="#" class="w3-bar-item w3-button w3-mobile">
+            Contacto
+        </a>
+
     </nav>
+
 </div>
 
 </body>
 </html>
+
