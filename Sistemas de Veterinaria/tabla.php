633a72916e1d81c267c9ecb941c681437a0714d8
Danielgatoinsano1 <danielddd173@gmail.com>
2026-07-29 17:56:24 +0200

    Agrega tabla de datos de ejemplo

---
+++ Sistemas de Veterinaria/tabla.php
@@ -0,0 +1,17 @@
+<table>
+    <tr>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Edad</th>
+    </tr>
+    <tr>
+        <td>Juan Carlos</td>
+        <td>Suazo</td>
+        <td>25</td>
+    </tr>
+    <tr>
+        <td>Lourdes</td>
+        <td>Flores</td>
+        <td>45</td>
+    </tr>
+</table>
